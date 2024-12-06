4bf168fc8f88352385f4ac723583aa3aac0b24e5
Umakonga12 <umakonga12@gmail.com>
2024-12-06 17:14:26 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @UmaMaheswariKonga
+- 👀 I’m interested in web development
+- 🌱 I’m currently learning html css
+- 💞️ I’m looking to collaborate on web design
+- 📫 How to reach me umakonga12@gmail.com
+
+<!---
+Umakonga12/Umakonga12 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
